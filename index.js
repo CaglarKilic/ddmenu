635985ba4c2a4f8e0d5f2c2634d02567076024b5
Caglar Kilic <73300418+CaglarKilic@users.noreply.github.com>
2023-12-29 10:43:27 +0300

    Finished form entry for item categories

---
+++ index.js
@@ -0,0 +1,92 @@
+const items = {};
+
+function selectCategoryTree() {
+  const main = document.querySelector("#main-category");
+  const sub = document.querySelector("#sub-category");
+  const item = document.querySelector("#item-category");
+
+  const mainIndex = main.selectedIndex;
+  const subIndex = sub.selectedIndex;
+
+  main.replaceChildren();
+  Object.keys(items).forEach((key) => {
+    main.add(new Option(key));
+  });
+  main.selectedIndex = mainIndex;
+
+  sub.replaceChildren();
+  if (main.value) {
+    Object.keys(items[main.value]).forEach((key) => {
+      sub.add(new Option(key));
+    });
+    sub.selectedIndex = subIndex;
+  }
+
+  item.replaceChildren();
+  if (sub.value) {
+    items[main.value][sub.value].forEach((elem) => {
+      item.add(new Option(elem));
+    });
+  }
+}
+
+function addSelect(select, options) {
+  select.replaceChildren();
+  options.forEach((option) => {
+    select.add(new Option(option));
+  });
+  document.forms[0].reset();
+}
+
+function addMainCategory(event) {
+  if (event.key === "Enter" && event.target.value) {
+    items[event.target.value] = {};
+    const select = document.querySelector("#main-category");
+    addSelect(select, Object.keys(items));
+    select.selectedIndex = select.options.length - 1;
+    selectCategoryTree();
+  }
+}
+
+function addSubCategory(event) {
+  if (event.key === "Enter" && event.target.value) {
+    const category = document.querySelector("#main-category").value;
+    items[category][event.target.value] = [];
+    const select = document.querySelector("#sub-category");
+    addSelect(select, Object.keys(items[category]));
+    select.selectedIndex = select.options.length - 1;
+    selectCategoryTree();
+  }
+}
+
+function addItemCategory(event) {
+  if (event.key === "Enter" && event.target.value) {
+    const mainCategory = document.querySelector("#main-category").value;
+    const subCategory = document.querySelector("#sub-category").value;
+    items[mainCategory][subCategory].push(event.target.value);
+    const select = document.querySelector("#item-category");
+    addSelect(select, items[mainCategory][subCategory]);
+    select.selectedIndex = select.options.length - 1;
+    selectCategoryTree();
+  }
+}
+
+document
+  .querySelector("#get-main-category")
+  .addEventListener("keypress", addMainCategory);
+
+document
+  .querySelector("#get-sub-category")
+  .addEventListener("keypress", addSubCategory);
+
+document
+  .querySelector("#get-item-category")
+  .addEventListener("keypress", addItemCategory);
+
+document
+  .querySelector("#main-category")
+  .addEventListener("change", selectCategoryTree);
+
+document
+  .querySelector("#sub-category")
+  .addEventListener("change", selectCategoryTree);
